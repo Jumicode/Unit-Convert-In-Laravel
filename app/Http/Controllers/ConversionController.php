ae9fb084c733b03ba8161ccfcd8142e27704c24b
Julio Romero <jromeroacive@gmail.com>
2024-10-02 15:49:13 -0400

    Length functions

--- app/Http/Controllers/ConversionController.php
+++ app/Http/Controllers/ConversionController.php
@@ -6,5 +6,30 @@ use Illuminate\Http\Request;
 
 class ConversionController extends Controller
 {
-    //
+    public function length()
+    {
+        return view('conversion.length');
+    }
+
+    public function convertLength(Request $request)
+    {
+        $value = $request->input('value');
+        $fromUnit = $request->input('from_unit');
+        $toUnit = $request->input('to_unit');
+
+        $conversions = [
+            'meter' => 1,
+            'kilometer' => 1000,
+            'centimeter' => 0.01,
+            'millimeter' => 0.001,
+            'inch' => 0.0254,
+            'foot' => 0.3048,
+            'yard' => 0.9144,
+            'mile' => 1609.34,
+        ];
+
+        $result = ($value * $conversions[$fromUnit]) / $conversions[$toUnit];
+
+        return view('conversion.length', compact('result', 'value', 'fromUnit', 'toUnit'));
+    }
 }
