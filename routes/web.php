2b35e53613d9516ec9dc8a5be3d19a4b89a1de00
Julio Romero <jromeroacive@gmail.com>
2024-10-02 15:42:23 -0400

    Web Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\ConversionController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/length',[ConversionController::class,'length'])->name('length');
+Route::post('/length',[ConversionController::class,'ConvertLength']);
+
+Route::get('/weigth',[ConversionController::class, 'weigth'])->name('weigth');
+Route::post('/weight', [ConversionController::class, 'convertWeight']);
+
+Route::get('/temperature', [ConversionController::class, 'temperature'])->name('temperature');
+Route::post('/temperature', [ConversionController::class, 'convertTemperature']);
+
+
+
+
+
